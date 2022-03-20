825c92739095149dada5e1b7189c50d3ebaed0a3
hphong <hp.hong@hotmail.com>
2022-03-21 06:22:26 +0900

    fourth commit

--- src/main/java/com/example/mongodb/MongodbApplication.java
+++ src/main/java/com/example/mongodb/MongodbApplication.java
@@ -1,17 +1,9 @@
 package com.example.mongodb;
 
-
-import java.util.ArrayList;
-import java.util.List;
-
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-import com.example.mongodb.model.WeatherItem;
-import com.example.mongodb.repository.ItemRepository;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
--- src/main/java/com/example/mongodb/repository/ItemRepository.java
+++ src/main/java/com/example/mongodb/repository/ItemRepository.java
@@ -2,9 +2,6 @@ package com.example.mongodb.repository;
 
 import com.example.mongodb.model.WeatherItem;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
-
-import java.util.List;
 
 public interface ItemRepository extends MongoRepository<WeatherItem, String> {
 
